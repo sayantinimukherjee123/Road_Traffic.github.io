0b9d3b5c59e69d6ad3744675027ba0462a0ae0d7
sayantinimukherjee79 <147259828+sayantinimukherjee79@users.noreply.github.com>
2023-11-16 19:19:19 +0530

    trafficroad

---
+++ admin/home.php
@@ -0,0 +1,75 @@
+<h1 class="text-light">Welcome to <?php echo $_settings->info('name') ?></h1>
+<hr class="border-light">
+<div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-light elevation-1"><i class="fas fa-th-list"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Category</span>
+                <span class="info-box-number">
+                  <?php 
+                   
+                  ?>
+                  <?php ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users-cog"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Staff</span>
+                <span class="info-box-number">
+                  <?php 
+                    
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+
+          <!-- fix for small devices only -->
+          <div class="clearfix hidden-md-up"></div>
+
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-th-list"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Services</span>
+                <span class="info-box-number">
+                <?php 
+                    
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-file-invoice"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Service Requests</span>
+                <span class="info-box-number">
+                <?php 
+                   
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+        </div>
---
+++ readme.txt
@@ -0,0 +1,33 @@
+Free Download Source Code "Online Vehicle Service Management System	"
+
+FIRST Download
+
+1.XAMPP
+
+2."TEXT EDITOR" NOTEPAD++ OR SUBLIME TEXT 3 / ETC.
+
+3"vehicle_service"
+
+4. Download the zip file/ download winrar
+
+5. Extract the file and copy "vehicle_service" folder
+
+6.Paste inside root directory/ where you install xammp local disk C: drive D: drive E: paste: (for xampp/htdocs, 
+
+7. Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+8. Create a database with name vehicle_service_db.SQL
+
+6. Import vehicle_service_db.sql file(given inside the zip package in SQL file folder)
+
+7.Run the script http://localhost/vehicle_service	
+
+**LOGIN DETAILS** 
+
+Admin
+user: admin
+pass: admin123	
+
+
+****** https:1sourcecodr.blogspot.com ******
+Subcribe my You tube Channel **** 1 Source code ****
